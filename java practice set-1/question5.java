965333156304d32b391a193fc33972b789597450
Vinayks99 <81642737+Vinayks99@users.noreply.github.com>
2021-03-31 00:26:31 +0530

    Update question5.java

--- java practice set-1/question5.java
+++ java practice set-1/question5.java
@@ -1,3 +1,4 @@
+//Write a program to assign a value of 100.235 to a double variable and then convert it to int.
 public class question5 {
     public static void main(String[] args) {
         double var = 100.235;
