f90c18ca1339c0f2917c29e718f3672afbabdabe
Vinayks99 <81642737+Vinayks99@users.noreply.github.com>
2021-04-05 08:51:58 +0530

    Update and rename question4.java to Question4.java

--- java practice set-1/Question4.java
+++ java practice set-1/Question4.java
@@ -1,5 +1,5 @@
 //Print the ASCII value of the character 'h'.
-public class question4 {
+public class Question4 {
     public static void main(String[] args) {
         char alphabet = 'h';
         System.out.println((int)alphabet);
