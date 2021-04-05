6e6329caca61884eafa4fb8e6b69fedbc8ac60b4
Vinayks99 <81642737+Vinayks99@users.noreply.github.com>
2021-04-05 08:45:11 +0530

    Update and rename question1.java to Question1.java

--- java practice set-1/Question1.java
+++ java practice set-1/Question1.java
@@ -1,5 +1,5 @@
 //Write a Java program to print an int, a double and a char on screen.
-public class question1 {
+public class Question1 {
     public static void main(String[] args) {
         int num = 97;
         double value = 147.954;
