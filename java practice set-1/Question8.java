838a7b6a2b80697331d0565e0e499ff3da820620
Vinayks99 <81642737+Vinayks99@users.noreply.github.com>
2021-04-05 08:54:30 +0530

    Update and rename question8.java to Question8.java

--- java practice set-1/Question8.java
+++ java practice set-1/Question8.java
@@ -1,5 +1,5 @@
 //Write a program to find the square of the number 3.9.
-public class question8 {
+public class Question8 {
     public static void main(String[] args) {
         double num = 3.9*3.9;
         System.out.println("Square of number 3.9 is: ");
