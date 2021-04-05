fe870c8d0abaea1cdf44617f3fbc1a5d0b06e622
Vinayks99 <81642737+Vinayks99@users.noreply.github.com>
2021-04-05 08:51:03 +0530

    Update and rename question3.java to Question3.java

--- java practice set-1/Question3.java
+++ java practice set-1/Question3.java
@@ -1,5 +1,5 @@
 //Write a program to print the product of the numbers 8.2 and 6.
-public class question3 {
+public class Question3 {
     public static void main(String[] args) {
         float prod = (float)8.2*6;
         System.out.println("Product of 8.2 and 6 is ");
